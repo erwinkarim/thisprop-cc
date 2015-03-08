0408b29c7bb2420e90723ac3eb547c75c1b6bbbd
erwinkarim <erwinkarim@gmail.com>
2015-03-09 00:19:38 +0800

    update the new listing pages

--- public/js/listings.js
+++ public/js/listings.js
@@ -1,1 +1,11 @@
-console.log('loaded listings.js');
+$(document).ready(function(){
+	
+	$('#show-review').on('shown.bs.tab', function(e){
+		console.log('review tab shown');
+		//update contents from 
+		$('#title-review').empty().append( $('#title-input').val() );
+		$('#address-review').empty().append( $('#address-input').val() );
+		$('#type-review').empty().append( $('#type-input').val() );
+		$('#description-review').empty().append( $('#description-input').val() );
+	});
+});
